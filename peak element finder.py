3672b3e436ebbb7d7a13cd3cd888cb110ba0acff
Sony A <118586143+Ayyalisony@users.noreply.github.com>
2024-06-15 19:56:15 +0530

    Add files via upload

---
+++ peak element finder.py
@@ -0,0 +1,10 @@
+n=input()
+a=list(map(int,input().split()))
+mp=-999
+for i in range(1,len(a)-1):
+    if a[i]>a[i-1] and a[i]>a[i+1]:
+        p=a[i]
+        if p>mp:
+            mp=p
+print(mp)            
+    
